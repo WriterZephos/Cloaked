704a8b722c92f016b20c665aa084cb9b9ba0d409
MainPC\Bryant <bryantreadmorrill@gmail.com>
2020-09-03 20:33:20 -0600

    Add sprite animations

---
+++ Assets/Effects/SpriteAnimation.cs
@@ -0,0 +1,28 @@
+﻿using Clkd.Main;
+using Clkd.Assets.Interfaces;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+
+namespace Clkd.Assets
+{
+    public class SpriteAnimation : AbstractTimedEffect
+    {
+        public List<SpriteCoordinate> Frames { get; set; }
+        public bool HoldLastFrame { get; set; }
+
+        // Constructors
+        public SpriteAnimation(
+            List<SpriteCoordinate> frames,
+            TimeSpan interval = default(TimeSpan),
+            TimeSpan durationLimit = default(TimeSpan),
+            int iterationLimit = 1,
+            bool holdLastFrame = true) : base(interval, durationLimit, iterationLimit)
+        {
+            Frames = frames;
+            HoldLastFrame = holdLastFrame;
+        }
+        
+    }
+}
---
+++ State/Effects/SpriteAnimationState.cs
@@ -0,0 +1,56 @@
+﻿using Clkd.Assets;
+using Clkd.Assets.Interfaces;
+using Clkd.Utilities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Clkd.State
+{
+    public class SpriteAnimationState : AbstractTimedEffectState
+    {
+        public SpriteAnimation Animation
+        {
+            get => (SpriteAnimation) Effect;
+            set => Effect = value;
+        }
+
+        public int CurrentFrame { get; set; }
+        public RenderableCoordinate RenderableCoordinate { get; set; }
+
+        public SpriteAnimationState(SpriteAnimation animation, int currentFrame = 0) 
+            : base(animation, canUpdate: true, canGetRenderables: true) 
+        {
+            CurrentFrame = currentFrame;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (TimedEffectUtility.ProgressEffect(gameTime, this))
+            {
+                CurrentFrame++;
+                if (CurrentFrame >= Animation.Frames.Count) CurrentFrame = 0;
+            }
+        }
+
+        public override List<Renderable> GetRenderables(RenderableCoordinate? renderableCoordinate = null)
+        {
+            if (Completed && !Animation.HoldLastFrame) return null;
+
+            SpriteCoordinate spriteCoordinate = Animation.Frames[CurrentFrame];
+            return new List<Renderable>() {
+                new Renderable(spriteCoordinate, RenderableCoordinate)
+            };
+        }
+
+        /// <summary>
+        /// Overrides AbstractTimedEffectState to reset additional properties.
+        /// Calls base.Reset().
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            CurrentFrame = 0;
+        }
+    }
+}
