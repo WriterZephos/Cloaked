73a442de9e8e50e51dc410a02d959d3dbcd7afdb
MainPC\Bryant <bryantreadmorrill@gmail.com>
2020-12-20 18:03:11 -0700

    Finish ValueGameState implementation

--- State/ValueGameState.cs
+++ State/ValueGameState.cs
@@ -10,38 +10,125 @@ namespace Clkd.State
 
     public class ValueGameState : AbstractComponent
     {
-        public Dictionary<string, bool> BoolFlags = new Dictionary<string, bool>();
-        public Dictionary<string, string> StringValues = new Dictionary<string, string>();
-        public Dictionary<string, int> IntValues = new Dictionary<string, int>();
-        public Dictionary<string, float> FloatValues = new Dictionary<string, float>();
-        public Dictionary<string, long> LongValues = new Dictionary<string, long>();
-        public Dictionary<string, char> CharValues = new Dictionary<string, char>();
-        public Dictionary<string, TimeSpan> TimeSpanValues = new Dictionary<string, TimeSpan>();
+        public Dictionary<string, bool> BoolFlags { get; set; }
+        public Dictionary<string, string> StringValues { get; set; }
+        public Dictionary<string, int> IntValues { get; set; }
+        public Dictionary<string, float> FloatValues { get; set; }
+        public Dictionary<string, long> LongValues { get; set; }
+        public Dictionary<string, char> CharValues { get; set; }
+        public Dictionary<string, TimeSpan> TimeSpanValues { get; set; }
 
-        public bool SetFlag(string flagName, bool value)
+        public bool SetBoolValue(string flagName, bool value)
         {
+            ensureDictionaryIsInitialized<bool>(BoolFlags);
             BoolFlags.TryGetValue(flagName, out bool previous);
             BoolFlags[flagName] = value;
             return previous;
         }
 
-        public bool GetBoolFlag(string flagName)
+        public bool GetBoolValue(string flagName)
         {
+            ensureDictionaryIsInitialized<bool>(BoolFlags);
             BoolFlags.TryGetValue(flagName, out bool flag);
             return flag;
         }
 
         public string SetStringValue(string valueName, string value)
         {
+            ensureDictionaryIsInitialized<string>(StringValues);
             StringValues.TryGetValue(valueName, out string previous);
             StringValues[valueName] = value;
             return previous;
         }
 
-        public bool GetStringValue(string valueName)
+        public string GetStringValue(string valueName)
         {
-            BoolFlags.TryGetValue(valueName, out bool val);
+            ensureDictionaryIsInitialized<string>(StringValues);
+            StringValues.TryGetValue(valueName, out string val);
             return val;
+        }
+
+        public int SetIntValue(string valueName, int value)
+        {
+            ensureDictionaryIsInitialized<int>(IntValues);
+            IntValues.TryGetValue(valueName, out int previous);
+            IntValues[valueName] = value;
+            return previous;
+        }
+
+        public int GetIntValue(string valueName)
+        {
+            ensureDictionaryIsInitialized<int>(IntValues);
+            IntValues.TryGetValue(valueName, out int val);
+            return val;
+        }
+
+        public float SetFloatValue(string valueName, float value)
+        {
+            ensureDictionaryIsInitialized<float>(FloatValues);
+            FloatValues.TryGetValue(valueName, out float previous);
+            FloatValues[valueName] = value;
+            return previous;
+        }
+
+        public float GetFloatValue(string valueName)
+        {
+            ensureDictionaryIsInitialized(FloatValues);
+            FloatValues.TryGetValue(valueName, out float val);
+            return val;
+        }
+
+        public long SetLongValue(string valueName, long value)
+        {
+            ensureDictionaryIsInitialized<long>(LongValues);
+            LongValues.TryGetValue(valueName, out long previous);
+            LongValues[valueName] = value;
+            return previous;
+        }
+
+        public long GetLongValue(string valueName)
+        {
+            ensureDictionaryIsInitialized(LongValues);
+            LongValues.TryGetValue(valueName, out long val);
+            return val;
+        }
+
+        public char SetCharValue(string valueName, char value)
+        {
+            ensureDictionaryIsInitialized<char>(CharValues);
+            CharValues.TryGetValue(valueName, out char previous);
+            CharValues[valueName] = value;
+            return previous;
+        }
+
+        public char GetCharValue(string valueName)
+        {
+            ensureDictionaryIsInitialized(CharValues);
+            CharValues.TryGetValue(valueName, out char val);
+            return val;
+        }
+
+        public TimeSpan SetTimeSpanValue(string valueName, TimeSpan value)
+        {
+            ensureDictionaryIsInitialized<TimeSpan>(TimeSpanValues);
+            TimeSpanValues.TryGetValue(valueName, out TimeSpan previous);
+            TimeSpanValues[valueName] = value;
+            return previous;
+        }
+
+        public TimeSpan GetTimeSpanValue(string valueName)
+        {
+            ensureDictionaryIsInitialized(TimeSpanValues);
+            TimeSpanValues.TryGetValue(valueName, out TimeSpan val);
+            return val;
+        }
+
+        private void ensureDictionaryIsInitialized<T>(Dictionary<string, T> dictionary)
+        {
+            if (dictionary == null)
+            {
+                dictionary = new Dictionary<string, T>();
+            }
         }
     }
 }
